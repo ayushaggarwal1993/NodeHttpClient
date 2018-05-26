601af65c2fb527b3496a6c00ef233dede13ad5b6
Ayush Aggarwal <ayushaggarwal170893@gmail.com>
2018-05-26 13:39:41 +0530

    Printing the response in UTF-8 format(human readable).Also showing no. of chunks.

--- http-get-no-buffs.js
+++ http-get-no-buffs.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
-const url = 'http://nodeprogram.com';
+const url = 'http://nodeprogram.com/';
 
 http.get(url, (response) =>{
+    let numOfChunks = 0;
     response.on('data', (chunk) =>{
-        console.log(chunk);
+        numOfChunks++;
+        console.log(chunk.toString('utf8'));
     })
     response.on('end', () =>{
-        console.log('Response has ended');
+        console.log(`Response has ended with ${numOfChunks} chunks`);
     })
 }).on('error', (error) => {
     console.log(`Got error: ${error}`);
